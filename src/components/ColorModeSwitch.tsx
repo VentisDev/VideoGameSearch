b11a303cd9b98cdbb6966d4664222f924e8cbac6
Ventis <tommylindev@gmail.com>
2023-09-06 10:37:52 -0700

    highlights the selected Genre

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -6,7 +6,7 @@ const ColorModeSwitch = () => {
   return (
     <HStack>
         <Switch colorScheme="green" isChecked={colorMode === "dark"} onChange={toggleColorMode}/>
-        <Text>Flip Color Mode</Text>
+        <Text>Color Mode</Text>
     </HStack>
   )
 }
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from "../services/image-crop-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null
@@ -18,7 +19,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-            <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant="link">{genre.name}</Button>
+            <Button onClick={() => onSelectGenre(genre)} fontSize='lg' fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"} variant="link">{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
