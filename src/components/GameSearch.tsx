235a90ba756ac70981b59fc9f8e9b4191af8c105
Ventis <tommylindev@gmail.com>
2023-09-11 10:56:34 -0700

    building game search input functionality

--- src/components/GameSearch.tsx
+++ src/components/GameSearch.tsx
@@ -1,17 +1,32 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const GameSearch = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const GameSearch = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-    <InputLeftElement children={<BsSearch />} />
-      <Input
-        placeholder="Search Games..."
-        variant="field"
-        borderRadius={20}
-        paddingY={3}
-      ></Input>
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value)
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          placeholder="Search Games..."
+          variant="field"
+          borderRadius={20}
+          paddingY={3}
+        ></Input>
+      </InputGroup>
+    </form>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import GameSearch from "./GameSearch";
 
-const Navbar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const Navbar = ({onSearch}: Props) => {
   return (
     <HStack padding="0px 10px">
       <Image src={logo} boxSize="60px" />
-      <GameSearch />
+      <GameSearch onSearch={onSearch}/>
       <ColorModeSwitch />
     </HStack>
   );
